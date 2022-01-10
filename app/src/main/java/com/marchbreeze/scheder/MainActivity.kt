ecaea26587bfae36705f6184bb5cf487127e29e2
MarchBreeze <marchbreeze03@gmail.com>
2022-01-10 18:23:08 +0900

    Set Toolbar

---
+++ app/src/main/java/com/marchbreeze/scheder/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.marchbreeze.scheder
+
+import android.graphics.Color
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.snackbar.Snackbar
+import com.marchbreeze.scheder.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setSupportActionBar(binding.toolbar)
+        supportActionBar?.setDisplayShowTitleEnabled(false)
+        window.statusBarColor = Color.parseColor("#FF3C3D59")
+    }
+
+
+}
