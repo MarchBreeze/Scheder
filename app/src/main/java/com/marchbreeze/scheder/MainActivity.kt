12ca0e462954d5085e7761fff51cff6c3c932e07
Sangho Kim <97405341+MarchBreeze@users.noreply.github.com>
2022-01-11 04:12:10 +0900

    Set TabLayout & ViewPager Design

--- app/src/main/java/com/marchbreeze/scheder/MainActivity.kt
+++ app/src/main/java/com/marchbreeze/scheder/MainActivity.kt
@@ -2,6 +2,7 @@ package com.marchbreeze.scheder
 
 import android.graphics.Color
 import android.os.Bundle
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.tabs.TabLayout
 import com.google.android.material.tabs.TabLayoutMediator
@@ -38,10 +39,10 @@ class MainActivity : AppCompatActivity() {
         // 탭 레이아웃과 뷰 페이져 연동
         TabLayoutMediator(binding.tabLayout, binding.viewPager) { tab, position ->
             when (position) {
-                0 -> tab.text = "Text1"
-                1 -> tab.text = "Text2"
-                2 -> tab.text = "Text3"
-                3 -> tab.text = "Text4"
+                0 -> tab.icon = getDrawable(R.drawable.icon_person)
+                1 -> tab.icon = getDrawable(R.drawable.icon_calendar)
+                2 -> tab.icon = getDrawable(R.drawable.icon_request)
+                3 -> tab.icon = getDrawable(R.drawable.icon_check)
             }
         }.attach()
     }
