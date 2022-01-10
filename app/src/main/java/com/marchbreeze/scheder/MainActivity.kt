511d22893a735c9b3e047956ceb5423579f38637
Sangho Kim <97405341+MarchBreeze@users.noreply.github.com>
2022-01-11 02:15:50 +0900

    Set TabLayout & ViewPager (without design)

--- app/src/main/java/com/marchbreeze/scheder/MainActivity.kt
+++ app/src/main/java/com/marchbreeze/scheder/MainActivity.kt
@@ -2,10 +2,9 @@ package com.marchbreeze.scheder
 
 import android.graphics.Color
 import android.os.Bundle
-import android.view.Menu
-import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
-import com.google.android.material.snackbar.Snackbar
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
 import com.marchbreeze.scheder.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -19,6 +18,32 @@ class MainActivity : AppCompatActivity() {
         setSupportActionBar(binding.toolbar)
         supportActionBar?.setDisplayShowTitleEnabled(false)
         window.statusBarColor = Color.parseColor("#FF3C3D59")
+
+        // 뷰 페이져에 어댑터 연결
+        val adapter = MainFragmentAdapter(this)
+        binding.viewPager.adapter = adapter
+
+        // 탭 설정
+        binding.tabLayout.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
+            override fun onTabSelected(tab: TabLayout.Tab?) {
+            }
+
+            override fun onTabUnselected(tab: TabLayout.Tab?) {
+            }
+
+            override fun onTabReselected(tab: TabLayout.Tab?) {
+            }
+        })
+
+        // 탭 레이아웃과 뷰 페이져 연동
+        TabLayoutMediator(binding.tabLayout, binding.viewPager) { tab, position ->
+            when (position) {
+                0 -> tab.text = "Text1"
+                1 -> tab.text = "Text2"
+                2 -> tab.text = "Text3"
+                3 -> tab.text = "Text4"
+            }
+        }.attach()
     }
 
 
