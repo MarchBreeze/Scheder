b92c50cecb669d7f4fa9e37a4082a397b41b5585
Sangho Kim <97405341+MarchBreeze@users.noreply.github.com>
2022-01-12 02:59:27 +0900

    Set Fragment_1 Design

---
+++ app/src/main/java/com/marchbreeze/scheder/MainRecyclerViewAdapter.kt
@@ -0,0 +1,26 @@
+package com.marchbreeze.scheder
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.marchbreeze.scheder.databinding.ItemWorkerBinding
+
+class MainViewHolder(val binding: ItemWorkerBinding) : RecyclerView.ViewHolder(binding.root)
+
+class MainRecyclerViewAdapter(val datas: MutableList<String>) :
+    RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+
+    override fun getItemCount(): Int = datas.size
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int):
+            RecyclerView.ViewHolder = MainViewHolder(
+        ItemWorkerBinding.inflate(
+            LayoutInflater.from(parent.context), parent, false
+        )
+    )
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        val binding = (holder as MainViewHolder).binding
+        binding.workerName.text = datas[position]
+    }
+}
